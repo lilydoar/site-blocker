af3748f22ee434d92751bd648bc41558e488c62e
lilydoar <lilydoar4@gmail.com>
2024-02-06 18:56:19 -0800

    HostsLine now contains the parsing logic of the hosts file

--- src/hosts.rs
+++ src/hosts.rs
@@ -4,59 +4,55 @@ use std::{
     path::PathBuf,
 };
 
-const START_MARKER: &str = "# BLOCKER_START";
-const END_MARKER: &str = "# BLOCKER_END";
-
 pub struct HostsInteractor {
-    hosts: PathBuf,
     pub blocked_sites: Vec<String>,
 }
 
 impl HostsInteractor {
     pub fn new(hosts: PathBuf) -> Result<Self, std::io::Error> {
-        match hosts.exists() {
-            false => Err(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                "Hosts file not found",
-            )),
-            true => {
-                let blocked_sites = Self::read_blocked_sites(&hosts)?;
-                Ok(Self {
-                    hosts,
-                    blocked_sites,
-                })
-            }
-        }
+        let blocked_sites = get_blocked_sites_from_hosts_file(&hosts)?;
+        Ok(Self { blocked_sites })
     }
 
     pub fn blocked_sites(&self) -> &Vec<String> {
         &self.blocked_sites
     }
 
-    pub fn add_site(&self, site: &str) -> Result<(), std::io::Error> {
+    pub fn add_site(&self, _site: &str) -> Result<(), std::io::Error> {
         todo!()
     }
 
-    pub fn remove_site(&self, site: &str) -> Result<(), std::io::Error> {
+    pub fn remove_site(&self, _site: &str) -> Result<(), std::io::Error> {
         todo!()
-    }
-
-    fn read_blocked_sites(hosts: &PathBuf) -> Result<Vec<String>, std::io::Error> {
-        // Return all sites between the START and END markers
-        todo!()
-    }
-
-    fn hosts_lines(&self) -> Result<Vec<HostsLine>, std::io::Error> {
-        // Retrieve the contents of the hosts file
-        let file = File::open(&self.hosts)?;
-        BufReader::new(file).lines().collect() // Need to handle the Result portion of this
     }
 }
 
+fn get_blocked_sites_from_hosts_file(hosts: &PathBuf) -> Result<Vec<String>, std::io::Error> {
+    Ok(hosts_file_lines(hosts)?
+        .into_iter()
+        .filter_map(|line| HostsLine::from(line).blocked_site())
+        .collect())
+}
+
+fn hosts_file_lines(hosts: &PathBuf) -> Result<Vec<String>, std::io::Error> {
+    BufReader::new(match File::open(hosts) {
+        Ok(file) => file,
+        Err(err) => {
+            return Err(std::io::Error::new(
+                err.kind(),
+                format!("Opening hosts file {}: {}", hosts.display(), err),
+            ));
+        }
+    })
+    .lines()
+    .collect()
+}
+
 enum HostsLine {
-    Comment(String),
-    Site(String),
     Empty,
+    Comment(String),
+    Entry(String, String),
+    Other(String),
 }
 
 impl From<String> for HostsLine {
@@ -64,33 +60,34 @@ impl From<String> for HostsLine {
         match line.trim() {
             "" => HostsLine::Empty,
             line if line.starts_with('#') => HostsLine::Comment(line.to_string()),
-            line => HostsLine::Site(line.to_string()),
+            line => {
+                let mut parts = line.split_whitespace();
+                let (ip, domain) = match (parts.next(), parts.next()) {
+                    (Some(ip), Some(domain)) => (ip, domain),
+                    _ => return HostsLine::Other(line.to_string()),
+                };
+                if parts.next().is_some() {
+                    return HostsLine::Other(line.to_string());
+                }
+                HostsLine::Entry(ip.to_string(), domain.to_string())
+            }
         }
     }
 }
 
-// Retrieve the contents of the hosts file and
-// filter out the lines that are not between the START and END markers
-//
-// let mut in_blocker_region = false;
-// let mut sites = Vec::new();
-// for line in self.hosts_lines()? {
-//     let line = line.trim();
-//     if line == START_MARKER {
-//         in_blocker_region = true;
-//         continue;
-//     }
-//
-//     if line == END_MARKER {
-//         break;
-//     }
-//
-//     if in_blocker_region {
-//         let site = line.split_whitespace().last().unwrap();
-//         sites.push(site.to_string());
-//     }
-//     // unfinished
-// }
-//
-//
-// Ok(sites)
+impl HostsLine {
+    fn blocked_site(&self) -> Option<String> {
+        if let HostsLine::Entry(ip, domain) = self {
+            if domain == "localhost" {
+                return None;
+            }
+
+            return match ip.as_str() {
+                "127.0.0.1" | "::1" => Some(domain.clone()),
+                _ => None,
+            };
+        }
+
+        None
+    }
+}
