cb00ef5f3e54f0830a9509a2b20c2fd1e9ea40bd
lilydoar <lilydoar4@gmail.com>
2024-02-06 18:56:19 -0800

    HostsLine now contains the parsing logic of the hosts file

--- src/command.rs
+++ src/command.rs
@@ -1,5 +1,7 @@
 use clap::Subcommand;
 
+use crate::hosts::HostsInteractor;
+
 #[derive(Subcommand)]
 pub enum Command {
     #[command(about = "List blocked sites")]
@@ -25,4 +27,45 @@ pub enum AddResponse {
 pub enum RemoveResponse {
     NotFound(String),
     Removed(String),
+}
+
+pub fn handle_command(
+    command: Command,
+    interactor: HostsInteractor,
+) -> Result<CommandResponse, std::io::Error> {
+    match command {
+        Command::List => Ok(CommandResponse::List(interactor.blocked_sites().to_vec())),
+        Command::Add { site } => match interactor.blocked_sites().contains(&site) {
+            true => Ok(CommandResponse::Add(AddResponse::AlreadyExists(site))),
+            false => {
+                interactor.add_site(&site)?;
+                Ok(CommandResponse::Add(AddResponse::Added(site)))
+            }
+        },
+        Command::Remove { site } => match interactor.blocked_sites().contains(&site) {
+            false => Ok(CommandResponse::Remove(RemoveResponse::NotFound(site))),
+            true => {
+                interactor.remove_site(&site)?;
+                Ok(CommandResponse::Remove(RemoveResponse::Removed(site)))
+            }
+        },
+    }
+}
+
+pub fn write_response(response: CommandResponse) {
+    match response {
+        CommandResponse::List(sites) => {
+            for site in sites {
+                println!("{}", site);
+            }
+        }
+        CommandResponse::Add(resp) => match resp {
+            AddResponse::AlreadyExists(site) => println!("{} is already in the block list", site),
+            AddResponse::Added(site) => println!("{} added to the block list", site),
+        },
+        CommandResponse::Remove(resp) => match resp {
+            RemoveResponse::NotFound(site) => println!("{} is not in the block list", site),
+            RemoveResponse::Removed(site) => println!("{} removed from the block list", site),
+        },
+    }
 }
